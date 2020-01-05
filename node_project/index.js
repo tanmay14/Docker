4b0655554e75d1273002d179fb6885ec2e1f684b
tanmay patel <tanmay.patel@kpn.com>
2020-01-05 21:21:11 +0100

    New : Initial Commit

---
+++ node_project/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+ 
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send('How are you doing');
+});
+ 
+app.listen(8888, () => {
+  console.log('Listening on port 8888');
+});
